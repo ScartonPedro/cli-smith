0bca6d2a2dd75fc67bf9f712608f889968a7f35e
Pedro Scarton <pedro@humanapis.io>
2023-02-18 09:27:09 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,15 @@
 #!/usr/bin/env node
-const path = require('path');
-const fs = require('fs');
 const yargs = require('yargs');
+const { getFile } = require('./helpers/readSmith');
 
 const argvs = yargs(process.argv.slice(2)).argv._;
 
 const [argv] = argvs;
 if (argv === 'setup') {
-  // Try with Json
-  const filePath = path.resolve('smith.json');
-  if (!fs.existsSync(filePath)) {
-    console.error(`Smith File not found on your current directory`);
-    return;
-  }
-
-  // Check if the file is a valid Smith file
   let data;
   try {
-    data = JSON.parse(fs.readFileSync(filePath));
+    data = getFile();
   } catch (err) {
-    console.error(`Error parsing '${filePath}': ${err.message}`);
     return;
   }
 
